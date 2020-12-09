7294dbeba71ac507d3645913b9fa8e0a4137be8d
BMAZUR <49879328+bmazurme@users.noreply.github.com>
2020-12-09 10:29:57 +0300

    #Update

--- Model/Test/Class1.cs
+++ Model/Test/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
@@ -11,29 +12,26 @@ namespace Entools.Model
         {
             UIDocument uidoc = revit.Application.ActiveUIDocument;
             Document doc = uidoc.Document;
-
-            int flag = 1;
-            string deltaNumber = string.Empty;
             string newString = string.Empty;
-            string message = string.Empty; 
+            bool flag = true;
 
+            // Skip checking for null.
             IList<Element> listAreas = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Rooms)
-                .WhereElementIsNotElementType().Where(a => a.LookupParameter("ROM_Зона").AsString().Contains("Квартира")).ToList();
+                                       .WhereElementIsNotElementType().Where(a => a.LookupParameter("ROM_Зона")
+                                       .AsString().Contains("Квартира")).ToList();
 
             List<Element> listAreasGroupZone = listAreas.OrderBy(x => x.get_Parameter(BuiltInParameter.LEVEL_NAME).AsString())
-                                                            .ThenBy(y => y.get_Parameter(BuiltInParameter.LEVEL_NAME).AsString())
-                                                            .ThenBy(a => a.LookupParameter("BS_Блок").AsString())
-                                                            .ThenBy(b => b.LookupParameter("ROM_Подзона").AsString())
-                                                            .ThenBy(c => c.LookupParameter("ROM_Зона").AsString())
-                                                            .ToList();
+                                                         .ThenBy(y => y.get_Parameter(BuiltInParameter.LEVEL_NAME).AsString())
+                                                         .ThenBy(a => a.LookupParameter("BS_Блок").AsString())
+                                                         .ThenBy(b => b.LookupParameter("ROM_Подзона").AsString())
+                                                         .ThenBy(c => c.LookupParameter("ROM_Зона").AsString())
+                                                         .ToList();
 
-            int length = listAreasGroupZone.Count();
-
-            using (Transaction t = new Transaction(doc, "add val par"))
+            using (Transaction t = new Transaction(doc, "Add value to parameter"))
             {
                 t.Start();
 
-                for (int i = 0; i < length - 1; i++)
+                for (int i = 0; i < listAreasGroupZone.Count() - 1; i++)
                 {
                     string levelFirst = listAreasGroupZone[i].get_Parameter(BuiltInParameter.LEVEL_NAME).AsString();
                     string levelSecond = listAreasGroupZone[i + 1].get_Parameter(BuiltInParameter.LEVEL_NAME).AsString();
@@ -49,26 +47,39 @@ namespace Entools.Model
 
                     if (levelFirst == levelSecond && sectionFirst == sectionSecond && typeFirst == typeSecond)
                     {
-                        if (numberFirst != numberSecond)
-                        {
-                            if (flag == 1)
+                        if (numberFirst != numberSecond)                        
+                            if (flag)
                             {
-                                flag = -1;
-                                newString = listAreasGroupZone[i + 1].LookupParameter("ROM_Расчетная_подзона_ID").AsString() + ".Полутон";
+                                flag = false;
+                                Parameter parameterId = listAreasGroupZone[i + 1].LookupParameter("ROM_Расчетная_подзона_ID");
+
+                                if (parameterId != null) newString = parameterId.AsString() + ".Полутон";
+                                else
+                                {
+                                    MessageBox.Show("ROM_Расчетная_подзона_ID == null");
+                                    break;
+                                }
                             }
                             else
                             {
-                                flag = 1;
+                                flag = true;
                                 newString = string.Empty;
-                            }
-                        }
+                            }                        
                     }
                     else
                     {
-                        flag = 1;
+                        flag = true;
                         newString = string.Empty;
                     }
-                    listAreasGroupZone[i + 1].LookupParameter("ROM_Подзона_Index").Set(newString);
+
+                    Parameter parameterIndex = listAreasGroupZone[i + 1].LookupParameter("ROM_Подзона_Index");
+
+                    if (parameterIndex != null) parameterIndex.Set(newString);
+                    else
+                    {
+                        MessageBox.Show("ROM_Подзона_Index == null");
+                        break;
+                    }
                 }
                 doc.Regenerate();
 
